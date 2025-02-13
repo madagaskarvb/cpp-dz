cf3d68adc99c5a97408993310175afeaaef21b9d
madagaskarvb <149654664+madagaskarvb@users.noreply.github.com>
2025-02-13 22:33:19 +0300

    Add files via upload

---
+++ dz1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+struct MyException {
+    std::string message; 
+
+    MyException(const std::string& msg) : message(msg) {}
+};
+
+void riskyFunction(int value) {
+    if (value < 0) {
+        throw MyException("Отрицательное значение недопустимо!");
+    }
+    std::cout << "Значение корректно: " << value << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    try {
+        riskyFunction(10);
+
+        riskyFunction(-5);
+    }
+    catch (const MyException& e) {
+        std::cerr << "Произошла ошибка: " << e.message << std::endl;
+    }
+
+    return 0;
+}
